6b31d8f5e1748751e1c0894fedc9db0a0e20b8d3
gw-dg <92924837+gw-dg@users.noreply.github.com>
2025-01-07 12:51:42 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,32 +11,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 console.log(process.env.FRONTEND_URL);
-
-const allowedOrigins = ["https://the-last-note-f2x7.vercel.app"];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Check if origin is in allowedOrigins
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
-  })
-);
-
-// Add OPTIONS handling for preflight requests
-app.options("*", (req, res) => {
-  res.status(204).send();
-});
-
+app.use(cors());
 const passportStrategies = require("./middlewares/passportStrategies");
 app.use((req, res, next) => {
   console.log(
@@ -62,4 +37,6 @@ app.post("/pastes", async (req, res) => {
   }
 });
 
-app.listen(3000);
+const PORT = 3000;
+
+app.listen(PORT);
